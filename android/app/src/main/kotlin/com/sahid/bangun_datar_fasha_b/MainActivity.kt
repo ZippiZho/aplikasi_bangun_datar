9f772ada83c4d5c6ad588ce8817210b6b7d61394
Fasha Dihiya <fashadhiya08@gmail.com>
2023-11-16 14:37:33 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/sahid/bangun_datar_fasha_b/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sahid.bangun_datar_fasha_b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page",
+            style: TextStyle(
+              color: Colors.white,
+            )),
+        backgroundColor: Color(0xFF1B1212),
+      ),
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              Expanded(child: CustomMenu(imageAsset: "assets/persegi23.jpeg",title: "Persegi",)),
+              Expanded(child: CustomMenu(imageAsset: "assets/segitiga-2.png",title: "Segitiga",)),
+            ],
+          ),
+
+          Row(
+            children: [
+              Expanded(child: CustomMenu(imageAsset: "assets/persegi-panjang.png",title: "Persegi Panjang",)),
+              Expanded(child: CustomMenu(imageAsset: "assets/lingkaran-23.png",title: "Lingkaran",)),
+            ],
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAsset, required this.title,
+  });
+
+  final String imageAsset;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      color: Colors.black12,
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Column(
+        children: [
+          Image.asset(imageAsset, height: 100,),
+          Text(
+           title,
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
